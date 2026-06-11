9d4dd25d57cc5e2ffecc9e686f29cc8a82287b5f
auraecosystem <sandraisom@icloud.com>
2026-06-11 23:22:45 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,1 +1,8 @@
-npx create-next-app@latest[qubuhub-incs(landing-page)nextjs-boilerplate-web4application.vercel
+npx create-next-app@latest[qubuhub-incs-landing-page] nuxtjs-boilerplate-web4application.vercel
+npx create-next-app@latest qubuhub-landing-page \
+  --ts \
+  --app \
+  --tailwind \
+  --eslint \
+  --src-dir \
+  --import-alias "@/*"
